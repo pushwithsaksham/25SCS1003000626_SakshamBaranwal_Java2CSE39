15af01f5f8225e7963d62087f89b682f3bc7ddcb
Saksham Baranwal <saksham.25scs1003000626@iilm.edu>
2026-08-08 15:05:22 +0530

    Update Sum.java

--- Assignment_3/Sum.java
+++ Assignment_3/Sum.java
@@ -1,1 +1,14 @@
+import java.util.*;
+public class Sum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first Number:");
+        int a = sc.nextInt();
+        System.out.print("Enter second Number:");
+        int b = sc.nextInt();
+        int Sum = a+b;
+        System.out.println("Sum = "+Sum);
+        sc.close();
+    }
+}
 
