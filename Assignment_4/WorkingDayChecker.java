927b83581b70e9711f39ff939b87bcd186022347
Saksham Baranwal <saksham.25scs1003000626@iilm.edu>
2026-08-27 23:13:12 +0530

    Create WorkingDayChecker.java

---
+++ Assignment_4/WorkingDayChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WorkingDayChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter day number (1-7): ");
+        int day = sc.nextInt();
+
+        if (day >= 1 && day <= 5)
+            System.out.println("Working Day");
+        else if (day == 6 || day == 7)
+            System.out.println("Weekend");
+        else
+            System.out.println("Invalid day");
+    }
+}
