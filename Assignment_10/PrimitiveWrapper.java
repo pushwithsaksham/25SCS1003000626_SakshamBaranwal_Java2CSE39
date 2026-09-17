5cf112a3776dc3e1625ec0590befa876c81d6fb2
Saksham Baranwal <saksham.25scs1003000626@iilm.edu>
2026-09-17 13:54:23 +0530

    Create PrimitiveWrapper.java

---
+++ Assignment_10/PrimitiveWrapper.java
@@ -0,0 +1,30 @@
+class PrimitiveWrapper
+{
+    public static void main(String[] args)
+    {
+        byte a = 10;
+        Byte b = 10;
+        short c = 20;
+        Short d = 20;
+        int e = 30;
+        Integer f = 30;
+        long g = 40L;
+        Long h = 40L;
+        float i = 50.5f;
+        Float j = 50.5f;
+        double k = 60.5;
+        Double l = 60.5;
+        char m = 'A';
+        Character n = 'A';
+        boolean o = true;
+        Boolean p = true;
+        System.out.println("byte: " + a + "  Byte: " + b);
+        System.out.println("short: " + c + "  Short: " + d);
+        System.out.println("int: " + e + "  Integer: " + f);
+        System.out.println("long: " + g + "  Long: " + h);
+        System.out.println("float: " + i + "  Float: " + j);
+        System.out.println("double: " + k + "  Double: " + l);
+        System.out.println("char: " + m + "  Character: " + n);
+        System.out.println("boolean: " + o + "  Boolean: " + p);
+    }
+}
